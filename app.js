9a4dd6be2ea8b2c5e7d62a92717be75d10dfd2a6
SeongEon <meanaccto@naver.com>
2021-02-22 01:42:28 +0900

    connect mysql

--- app.js
+++ app.js
@@ -14,4 +14,12 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/new', (req ,res) => {
+  res.render('new');
+});
+
+app.post('/create', (req,res) => {
+  
 })
